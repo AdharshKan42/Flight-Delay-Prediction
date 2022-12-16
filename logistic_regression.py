773593b8d6c9792d05b55e7d6a10b8d4f8f8c730
Vatsal Mehta <vatsalmehta@Vatsals-MacBook-Air.local>
2022-12-16 14:58:48 -0500

    added training time metrics

--- logistic_regression.py
+++ logistic_regression.py
@@ -3,19 +3,28 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from data_processing import process_data
 
-print("Preparing data")
-X_train, X_test, y_train, y_test, classes_mapping = process_data(
+from time import perf_counter
+
+print("Preparing data...")
+X_train, X_test, y_train, y_test, categories_mapping = process_data(
     "star_classification.csv"
 )
 
-print("Training model")
+print("Training model...")
 model = LogisticRegression()
+
+training_start_time = perf_counter()
+
 model.fit(X_train, y_train)
+
+training_end_time = perf_counter()
+
+print(f"Model took {training_end_time - training_start_time} seconds to train.")
 
 y_pred = model.predict(X_test)
 
 test_accuracy = model.score(X_test, y_test)
-print(f"Accuracy: {test_accuracy * 100}%")
+print(f"Test Accuracy: {test_accuracy * 100}%")
 
 sns.pairplot(X_test)
 
