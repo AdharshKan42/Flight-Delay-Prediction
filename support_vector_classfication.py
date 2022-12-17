083d80a73f2e9ed70ca2863f4a837a5cafa332e9
Vatsal Mehta <vatsalmehta@Vatsals-MacBook-Air.local>
2022-12-16 20:11:27 -0500

    accuracy message formatting changes

--- support_vector_classfication.py
+++ support_vector_classfication.py
@@ -16,9 +16,9 @@ def classify_points(X_train, X_test, y_train, y_test):
 
     print(f"Model took {training_end_time - training_start_time:.2f} seconds to train.")
 
-    print(f"Test Classification Error: {1 - model.score(X_test, y_test):.2f}")
-    print(f"Train Classification Accuracy: {model.score(X_train, y_train):.2f}")
-    print(f"Test Classification Accuracy: {model.score(X_test, y_test):.2f}")
+    print(f"Test Classification Error: {(1 - model.score(X_test, y_test))*100:.2f}")
+    print(f"Train Classification Accuracy: {(model.score(X_train, y_train))*100:.2f}")
+    print(f"Test Classification Accuracy: {(model.score(X_test, y_test))*100:.2f}")
 
     predictions = model.predict(X_test)
 
@@ -36,7 +36,6 @@ X_train, X_test, y_train, y_test, categories_mapping = process_data(
     "star_classification.csv"
 )
 
-print("Running SVM on Dataset 1")
+print("Running SVM")
 
-# run program
 classify_points(X_train, X_test, y_train, y_test)
